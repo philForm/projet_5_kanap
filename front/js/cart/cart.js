c0306a9cd93920318d89f0a96aa052da04758ea5
Philippe <phil-form@orange.fr>
2022-09-01 19:58:37 +0200

    arrangement du code de cart.js

--- front/js/cart/cart.js
+++ front/js/cart/cart.js
@@ -16,6 +16,10 @@ window.onload = () => {
 
     fetch(promise)
         .then(data => data.json())
+        .catch((err) => {
+            console.error(err);
+            alert(`Les details de votre panier ne peuvent être affichés ! ${err}`)
+        })
         .then(jsonArticle => {
             console.log(jsonArticle);
 
@@ -26,75 +30,85 @@ window.onload = () => {
             const tabArticle = funcTabArticle(jsonArticle, recupLocalStorage);
             console.log(tabArticle);
 
-            return tabArticle;
-
-        }).catch((err) => {
-            console.error(err);
-            alert(`Les details de votre panier ne peuvent être affichés ! ${err}`)
-
-        }).then(tabArticle => {
-
             // Affichage des articles sur la page
             displayArticlesOnPage(tabArticle, cartItemsElt, totalQuantityElt, totalPriceElt);
 
             for (let item of tabArticle) {
 
-                // index de item
-                let indexItem = tabArticle.indexOf(item);
+                /**
+                 * index de l'article dans tabArticle
+                 */
+                const indexItem = tabArticle.indexOf(item);
 
-                // Sélection de l'input indiquant la quantité
+                /**
+                 * Sélection de l'input indiquant la quantité
+                 * @type {HTMLInputElement} type number
+                 */
                 const inputValue = document.querySelector(`#cart__items > article:nth-child(${indexItem + 1}) input`);
 
-                // sélection de la balise <p> de quantité
-                const displayValue = document.querySelector(`#quantity-${indexItem + 1}`)
+                /**
+                 * sélection de la balise \<p\> de quantité
+                 * @type {HTMLParagraphElement}
+                 */
+                const displayValue = document.querySelector(`#quantity-${indexItem + 1}`);
 
-                // bouton de suppression d'un élément
-                let deleteItem = document.getElementById(`delete-${indexItem + 1}`);
-                console.log(deleteItem);
-                console.log(item[0]);
-                console.log(item[1]);
+                /**
+                 * sélection de la balise \<article\>
+                 * @type {HTMLElement}
+                 */
+                const children = document.getElementById(`art-${indexItem + 1}`);
+                console.log(children);
 
+                /**
+                 * bouton de suppression d'un élément
+                 * @type {HTMLButtonElement}
+                 */
+                const deleteItem = document.getElementById(`delete-${indexItem + 1}`);
+                
+                /**
+                 * id de l'article contenu dans data-id de la balise \<article\>
+                 */
+                const datasetId = inputValue.closest(`.cart__item`).dataset.id;
 
-                // dataset correspondant à l'item
-                let datasetId = inputValue.closest(`.cart__item`).dataset.id;
-                let datasetColor = inputValue.closest(`.cart__item`).dataset.color;
+                /**
+                 * couleur de l'article contenu dans data-color de la balise \<article\>
+                 */
+                const datasetColor = inputValue.closest(`.cart__item`).dataset.color;
+
                 inputValue.setAttribute("data-color", item[1]);
 
-                inputValue.addEventListener("change", (e) => {
+                /**
+                 * recherche de l'article dans tabArticle
+                 */
+                const art = tabArticle.find(el => el[0] == datasetId && el[1] == datasetColor)
+                console.log(art)
 
-                    // recherche de l'article dans tabArticle
-                    let art = tabArticle.find(el => el[0] == datasetId && el[1] == datasetColor)
-                    console.log(art)
+                inputValue.addEventListener("change", (e) => {
 
                     // Changement de quantité dans le localStorage
                     changeQuantity(art, e.target.value);
 
-                    if (e.target.value <= 0) {
-
-                        let children = document.getElementById(`art-${indexItem + 1}`);
-                        console.log(children);
-
+                    if (e.target.value <= 0) 
                         confirmRemoveProduct(art, children, inputValue, cartItemsElt);
 
-                    }
                     if (e.target.value > 100) {
-                        e.target.value = "100"
+                        e.target.value = "100";
                         changeQuantity(art, e.target.value);
                     }
 
                     // Affichage de la nouvelle quantité sur la page
                     displayValue.innerText = `Qté : ${e.target.value}`;
 
-                    quantityAndPrice(totalQuantityElt, totalPriceElt, tabArticle)
+                    quantityAndPrice(totalQuantityElt, totalPriceElt, tabArticle);
                 })
 
 
                 deleteItem.addEventListener('click', () => {
 
-                    confirmRemoveProduct(item, deleteItem.closest(`.cart__item`), inputValue, cartItemsElt)
+                    confirmRemoveProduct(item, deleteItem.closest(`.cart__item`), inputValue, cartItemsElt);
                     console.log(deleteItem.closest(`.cart__item`));
 
-                    quantityAndPrice(totalQuantityElt, totalPriceElt, tabArticle)
+                    quantityAndPrice(totalQuantityElt, totalPriceElt, tabArticle);
 
                 })
             }
